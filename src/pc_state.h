c57f97b3dade7326f38454894dcaa84995c10012
Artem <3954911+RottenC@users.noreply.github.com>
2026-07-21 13:38:25 +0300

    Define explicit power-hold transition requests

--- src/pc_state.h
+++ src/pc_state.h
@@ -12,16 +12,23 @@ public:
   bool startupRequested() const { return startupRequested_; }
   bool shutdownRequested() const { return shutdownRequested_; }
   bool resetRequested() const { return resetRequested_; }
-  bool forcedPreview() const { return forcedPreview_; }
+  bool holdPreviewRequested() const { return holdPreviewRequested_; }
+  bool cancelHoldPreviewRequested() const { return cancelHoldPreviewRequested_; }
   bool forcedLatched() const { return forcedLatched_; }
   uint32_t powerHoldStartMs() const { return powerHoldStartMs_; }
-  void clearRequests();
   void markStartupComplete();
-  void markShutdownComplete();
 private:
+  void clearRequests();
   PcState state_=PcState::Off;
+  uint32_t bootAtMs_=0;
   uint32_t powerHoldStartMs_=0;
-  bool trackingHold_=false, forcedPreview_=false, forcedLatched_=false;
-  bool startupRequested_=false, shutdownRequested_=false, resetRequested_=false;
+  bool initialized_=false;
+  bool trackingHold_=false;
+  bool forcedLatched_=false;
+  bool startupRequested_=false;
+  bool shutdownRequested_=false;
+  bool resetRequested_=false;
+  bool holdPreviewRequested_=false;
+  bool cancelHoldPreviewRequested_=false;
 };
 const __FlashStringHelper *pcStateName(PcState state);
